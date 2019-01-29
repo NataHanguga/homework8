9f05e4be67caf790caebd72736639ed20fa7d95f
NataHanguga <ptahakaimanka@yandex.ru>
2019-01-29 15:11:38 +0200

    v.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserComponent } from './components/user/user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './components/register/register.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from './services/auth.service';
+import { TaskCardComponent } from './components/task-card/task-card.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    RegisterComponent,
+    TodoComponent,
+    UpdateUserComponent,
+    TaskCardComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MDBBootstrapModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    CookieService,
+    AuthService
+  ],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MustMatch } from 'src/app/services/must-match.validator';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/services/user';
+import { CookieService } from 'ngx-cookie-service';
+import { ValidatorsRequared } from 'src/app/services/validatorsRequared';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  name = '^\w{1,}$';
+  email = '^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(('
+  + '\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  phone = '/^\(?([0-9]{3})\)?[-]?([0-9]{3})[-]?([0-9]{4})$/'; // '^\+?([0-9]   {2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$';
+  password = '^\w{5,20}$ ';
+  registerForm: FormGroup;
+  submitted = false;
+  temporaryUserId = 'eyJhbGciOiJIUzI1NiJ9.bmV3dGVzdHVzZXJfbmV3dGVzdHBhc3M.6G0GjokS52wciQthCjbg1lxIBc_2euvZjhgw5igtZ8Y';
+   user: User;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private auth: AuthService,
+              private cookieService: CookieService) { }
+
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          firstName: ['', Validators.required], // pattern(this.name)],
+          lastName: ['', Validators.required], // pattern(this.name)],
+          email: ['', Validators.required], // pattern(this.email)],
+          phone: ['', Validators.required], // pattern(this.phone)],
+          password: ['', [Validators.required, Validators.minLength(5)]], // pattern(this.password)],
+          confirmPassword: ['', Validators.required] // pattern(this.password)],
+      }, {
+          validator: MustMatch('password', 'confirmPassword')
+      });
+    }
+
+  regisrer() {
+    this.auth.registerToken(this.registerForm.value).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      },
+      () => {
+        console.log('complite register');
+      });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      this.cookieService.set(this.temporaryUserId, JSON.stringify(this.registerForm.value));      this.regisrer();
+      this.router.navigate(['main']);
+  }
+}
